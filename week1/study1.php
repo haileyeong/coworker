2927cddaae123f593c7f8460fa97892dd5756e82
hailey <haileyeong@gmail.com>
2024-10-29 14:10:07 +0900

    php guide 1

--- week1/study1.php
+++ week1/study1.php
@@ -1,11 +1,33 @@
-<?php
-print "<h3>1 - 100 까지 수 중에서 4와 6의 공배수</h3>";
-for ($i = 1; $i <= 100; $i++) {
-    if ($i % 4 == 0 && $i % 6 == 0) {
-        echo $i . ", ";
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>구구단 표</title>
+</head>
+<body>
+    <?php
+    echo "<h3>1 - 100 까지 수 중에서 4와 6의 공배수</h3>";
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 4 == 0 && $i % 6 == 0) {
+            echo $i . ", ";
+        }
     }
-}
+    echo "<br>";
 
+    echo "<h3>구구단 출력</h3>";
+    for ($i = 1; $i <= 9; $i += 3) { // 3단씩 끊어서.
+        for ($j = 1; $j <= 9; $j++) { // 단
+            for ($k = $i; $k < $i + 3; $k++) {
+                if ($k <= 9) {
+                    echo $k . " x " . $j . " = " . ($k * $j) . "\t";
+                }
+            }
+            echo "<br>";
+        }
+        echo "<br>";
+    }
 
+    ?>
+</body>
+</html>
 
-?>
