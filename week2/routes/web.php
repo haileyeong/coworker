25ea1d0f6335356bf95d14f69ebc040e05678cb5
hailey <haileyeong@gmail.com>
2024-11-11 16:19:10 +0900

    chore: 불필요한 공백 제거

--- week2/routes/web.php
+++ week2/routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\BoardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -19,8 +15,8 @@ require __DIR__.'/auth.php';
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::resource('boards', BoardController::class);
+
 Route::get('/board-list', [BoardController::class, 'index'])->name('boards.index');
 Route::get('/board', [BoardController::class, 'board'])->name('boards.board');
 Route::get('/boards/{id}', [BoardController::class, 'show'])->name('boards.show');
-
-Route::resource('boards', BoardController::class);
